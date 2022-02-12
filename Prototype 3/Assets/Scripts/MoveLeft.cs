e91aab96be49299cecf88a8c38c8e560d6fa3fec
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-02-12 19:03:47 +0100

    Unit 3 - Lesson 3.2 - step 5

--- Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30.0f;
+    private PlayerController _playerControllerScript;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Find the "Player" gameObject and get the playerController script from that player.
+        _playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (_playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
     }
 }
