620ed157a09341f1de72299f0786707ebaae801f
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-02-12 19:54:18 +0100

    Unit 3 - Lesson 3.3 - step 5

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -41,6 +41,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Game Over");
             gameOver = true;
+            _playerAnim.SetBool("Death_b", true);
+            _playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
