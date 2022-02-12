d87d32398e174315288f2ab46c7fe80f824e43e3
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-02-12 17:29:12 +0100

    Unit 3 - Lesson 3.1 - step 9

--- Prototype 3/Assets/Scripts/SpawnManager.cs
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private Vector3 _spawnPos = new Vector3(25, 0, 0);
+    private float _startDelay = 2.0f;
+    private float _repeatRate = 2.0f;
     
     // Start is called before the first frame update
     void Start()
     {
-        // Spawn a new obstacle
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating(nameof(SpawnObstacle), _startDelay, _repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle()
+    {
+        // Spawn a new obstacle
+        Instantiate(obstaclePrefab, _spawnPos, obstaclePrefab.transform.rotation);
     }
 }
