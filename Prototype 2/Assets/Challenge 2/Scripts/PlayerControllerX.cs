0205dc798b74dec991d6781002a71e27fdaa6c92
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-02-12 15:26:31 +0100

    Unit 2 - Challenge 2 - step 9

--- Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float timeBetweenKeyPresses = 1.0f;
+    public float keyPressTimer = 0.0f;
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        // On spacebar press && timer less then set delay time, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && keyPressTimer >= timeBetweenKeyPresses)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            // Reset the timer to 0
+            keyPressTimer = 0;
         }
+        
+        // Increase the timer
+        keyPressTimer += 1 * Time.deltaTime;
     }
 }
