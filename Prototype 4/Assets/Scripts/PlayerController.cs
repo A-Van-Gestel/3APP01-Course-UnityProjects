1c504a7ed052a17021ba1f81478ef7536d1c122f
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-02-13 16:08:27 +0100

    Unit 4 - Lesson 4.1 - Step 5

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody _playerRb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        _playerRb.AddForce(Vector3.forward * forwardInput * speed);
+    }
+}
