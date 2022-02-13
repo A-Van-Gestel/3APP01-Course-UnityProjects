3325c05e269fa5ba391cd28a2ebafbd7e57ccbd6
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-02-13 17:09:45 +0100

    Unit 4 - Lesson 4.2 - Step 2

---
+++ Prototype 4/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private Rigidbody _enemyRb;
+    private GameObject _player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyRb = GetComponent<Rigidbody>();
+        _player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Add force to the calculated vector (direction to the player)
+        _enemyRb.AddForce((_player.transform.position - transform.position).normalized * speed);
+    }
+}
