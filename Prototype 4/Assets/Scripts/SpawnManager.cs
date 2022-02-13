33c25671b634d4066ab14688434fdf2e957117d4
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-02-13 17:36:14 +0100

    Unit 4 - Lesson 4.2 - Step 5

--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    private float _spawnRange = 9.0f;
     
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+        // Generate a random spawn position
+        float spawnPosX = Random.Range(-_spawnRange, _spawnRange);
+        float spawnPosZ = Random.Range(-_spawnRange, _spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        
+        Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
