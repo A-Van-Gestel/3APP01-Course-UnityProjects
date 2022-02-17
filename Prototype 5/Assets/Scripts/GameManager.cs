7882134ade6d7b8fd9423c32c16edf203ebdf791
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-02-17 19:33:39 +0100

    Unit 5 - Lesson 5.2 - Step 3

--- Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    private int _score;
     private float _spawnRate = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        _score = 0;
+        scoreText.text = "Score: " + _score;
     }
 
     // Update is called once per frame
