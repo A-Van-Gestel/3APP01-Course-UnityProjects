9a20753406453a5e7f376ea6fd356e83e27c7d5a
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-02-17 19:37:39 +0100

    Unit 5 - Lesson 5.2 - Step 4

--- Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -15,7 +15,7 @@ public class GameManager : MonoBehaviour
     {
         StartCoroutine(SpawnTarget());
         _score = 0;
-        scoreText.text = "Score: " + _score;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -31,6 +31,14 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(_spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            
+            UpdateScore(5);
         }
+    }
+
+    void UpdateScore(int scoreToAdd)
+    {
+        _score += scoreToAdd;
+        scoreText.text = "Score: " + _score;
     }
 }
